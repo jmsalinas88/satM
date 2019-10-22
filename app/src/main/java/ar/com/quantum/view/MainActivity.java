be25e18c97b130b6269480232667b155df013690
Juan M. Salinas <jmsalinas88@gmail.com>
2019-10-21 22:31:46 -0300

    Agrego Pantalla de test.

---
+++ app/src/main/java/ar/com/quantum/view/MainActivity.java
@@ -0,0 +1,16 @@
+package ar.com.quantum.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ar.com.quantum.satm.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
