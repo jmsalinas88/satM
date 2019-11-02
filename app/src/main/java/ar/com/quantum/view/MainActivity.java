03d83a54d06f12546e1adafa90d433ed9ed8c595
Juan M. Salinas <jmsalinas88@gmail.com>
2019-11-02 16:48:39 -0300

    - Welcome Activity. - SingIn Activity.

--- app/src/main/java/ar/com/quantum/view/MainActivity.java
+++ app/src/main/java/ar/com/quantum/view/MainActivity.java
@@ -1,16 +1,28 @@
 package ar.com.quantum.view;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.os.Handler;
 import ar.com.quantum.satm.R;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static long WELCOME_TIMEOUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent(MainActivity.this, SingInActivity.class);
+                startActivity(welcome);
+                finish();
+            }
+        },  WELCOME_TIMEOUT);
+
     }
 }
---
+++ app/src/main/java/ar/com/quantum/view/SingInActivity.java
@@ -0,0 +1,70 @@
+package ar.com.quantum.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.com.quantum.satm.R;
+
+public class SingInActivity extends AppCompatActivity implements OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.singinactivity);
+
+        // Spinner element
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
+
+        // Spinner Drop down elements
+        List<String> categories = new ArrayList<String>();
+
+        categories.add("Ciudad Autonoma de Buenos Aires");
+        categories.add("Alberti");
+        categories.add("Villa Maria");
+        categories.add("Claypole");
+        categories.add("Jose Marmol");
+        categories.add("Longchamps");
+        categories.add("Rafael Calzada");
+        categories.add("Avellaneda");
+        categories.add("Crucesita");
+        categories.add("Sarandi");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter <String>(this, R.layout.localityitem, categories);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
